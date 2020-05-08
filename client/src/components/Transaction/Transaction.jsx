1fdceec1abeb3fd4b05ae2dbdd0e1e9d0d9ba070
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-05-08 17:50:51 +0530

    Initial commit

---
+++ client/src/components/Transaction/Transaction.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
+import Format from "../../utils/Format";
+
+const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+  const sign = transaction.amount < 0 ? "-" : "+";
+  return (
+    <div>
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
+        {transaction.text}
+        <span>
+          {sign}${Format(Math.abs(transaction.amount))}
+        </span>
+        <button
+          onClick={() => deleteTransaction(transaction._id)}
+          className="delete-btn"
+        >
+          x
+        </button>
+      </li>
+    </div>
+  );
+};
+
+export default Transaction;
