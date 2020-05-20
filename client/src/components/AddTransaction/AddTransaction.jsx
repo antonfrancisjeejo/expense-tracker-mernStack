5eafaf88a1d35f94b22e37aabaf7831235bc649e
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-05-20 20:55:12 +0530

    Version update

--- client/src/components/AddTransaction/AddTransaction.jsx
+++ client/src/components/AddTransaction/AddTransaction.jsx
@@ -1,21 +1,54 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { GlobalContext } from "../../context/GlobalState";
+import axios from "axios";
 
 import "./AddTransaction.css";
 
 const AddTransaction = () => {
-  const { addTransaction } = useContext(GlobalContext);
+  const { addTransaction, currentUser, updateTransaction } = useContext(
+    GlobalContext
+  );
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+  const [add, setAdd] = useState(0);
+
+  useEffect(() => {
+    getTransactions(currentUser.uid);
+  }, [currentUser.uid]);
+
+  const getTransactions = async (id) => {
+    try {
+      const res = await axios.get(
+        `https://expense-tracker-1306.herokuapp.com/api/v1/transactions/${id}`
+      );
+      const hey = res.data.data;
+
+      setAdd(Object.keys(hey).length);
+    } catch (err) {
+      alert(err);
+    }
+  };
 
   const onSubmit = (event) => {
     event.preventDefault();
     const newTransaction = {
-      id: Math.floor(Math.random() * 100000000),
-      text,
-      amount: Number(amount),
+      _id: currentUser.uid,
+      transactions: [
+        {
+          text,
+          amount: Number(amount),
+        },
+      ],
     };
-    addTransaction(newTransaction);
+    if (add !== 3) {
+      addTransaction(newTransaction);
+    } else {
+      const newTransaction = {
+        text,
+        amount: Number(amount),
+      };
+      updateTransaction(currentUser.uid, newTransaction);
+    }
     setAmount(0);
     setText("");
   };
